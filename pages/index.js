719a04a2e6ae5e40758f06da3aa9f78d889f649b
Diogo Ribeiro da Silva <diogoribeirosilva@gmail.com>
2023-01-14 18:56:56 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function HomePage() {
+    const mensagem = "AluraTube"
+    const estilosDaHomePage = { background: "red" }
+    return <div style={estilosDaHomePage}>{mensagem}</div>
+}
+
+export default HomePage
+
+function Menu() {
+    return {
+        < div >
+        Menu
+        </div >
+    }
+  }
